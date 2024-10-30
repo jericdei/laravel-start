99e5c18692251de0f23728220e59c4ba04f36ee8
Jeric June Logan <loganjeric25@gmail.com>
2024-10-31 02:34:01 +0800

    install shadcn and created sample pages

--- resources/js/pages/another.tsx
+++ resources/js/pages/another.tsx
@@ -1,11 +1,17 @@
-import { Link } from '@inertiajs/react';
+import { Button } from '@/components/ui/button';
+import { Head, Link } from '@inertiajs/react';
 
 export default function Another() {
   return (
     <div className="grid h-screen w-screen place-items-center">
-      <div className="text-center">
+      <div className="flex flex-col items-center gap-4">
+        <Head title="Another" />
+
         <p>This is the Another page.</p>
-        <Link href="/">Go to home</Link>
+
+        <Link href="/">
+          <Button>Go to home</Button>
+        </Link>
       </div>
     </div>
   );
